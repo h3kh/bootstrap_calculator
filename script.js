894314ef07f93eac45e8d4e5ad750e6ba297611a
hossein <hosseinkhakpour20@gmail.com>
2023-07-15 10:47:34 +0330

    completed script.js file

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 'use strict';
+const buttonsEl = document.querySelectorAll('button');
+
+const inputFieldEl = document.getElementById('result');
+
+for (let i = 0; i < buttonsEl.length; i++) {
+  buttonsEl[i].addEventListener('click', () => {
+    const buttonValue = buttonsEl[i].textContent;
+    if (buttonValue === 'C') {
+      clearResult();
+    } else if (buttonValue === '=') {
+      calculateResult();
+    } else {
+      appendValue(buttonValue);
+    }
+  });
+}
+
+function clearResult() {
+  inputFieldEl.value = '';
+}
+
+function calculateResult() {
+  const expression = inputFieldEl.value;
+  let result;
+
+  if (expression.includes('+')) {
+    const operands = expression.split('+');
+    // The parseFloat() function is used to parse a string and convert it to a floating-point number.
+    result = parseFloat(operands[0]) + parseFloat(operands[1]);
+  } else if (expression.includes('-')) {
+    const operands = expression.split('-');
+    result = parseFloat(operands[0]) - parseFloat(operands[1]);
+  } else if (expression.includes('*')) {
+    const operands = expression.split('*');
+    result = parseFloat(operands[0]) * parseFloat(operands[1]);
+  } else if (expression.includes('/')) {
+    const operands = expression.split('/');
+    result = parseFloat(operands[0]) / parseFloat(operands[1]);
+  }
+
+  inputFieldEl.value = result;
+}
+
+function appendValue(buttonValue) {
+  inputFieldEl.value += buttonValue;
+}
