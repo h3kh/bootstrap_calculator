629eb66aca8ba64b9127565fdde2cb84f2cd6a18
hossein <hosseinkhakpour20@gmail.com>
2023-07-18 14:09:16 +0330

    added condition and  deleted some comments

--- script.js
+++ script.js
@@ -31,8 +31,6 @@ function calculateResult() {
 
   if (expression.includes('+')) {
     const operands = expression.split('+');
-
-    // The parseFloat() function is used to parse a string and convert it to a floating-point number.
     result = parseFloat(operands[0]) + parseFloat(operands[1]);
   } else if (expression.includes('-')) {
     const operands = expression.split('-');
@@ -45,9 +43,6 @@ function calculateResult() {
     result = parseFloat(operands[0]) / parseFloat(operands[1]);
   } else if (expression.includes('^')) {
     const operands = expression.split('^');
-
-    // The Math.pow() function is used to calculate the power of a number to another specified power.
-    // In this line, raises operands[0] to the power of operands[1]
     result = Math.pow(parseFloat(operands[0]), parseFloat(operands[1]));
   }
 
@@ -55,6 +50,7 @@ function calculateResult() {
 
   // Save the expression and result to the history
   history.push({ expression, result });
+
   // Update the display of the history
   updateHistory();
 }
@@ -68,18 +64,19 @@ function updateHistory() {
   // Clear the display of the history
   historyEl.textContent = '';
 
-  // The forEach() method calls a function for each element in an array.
-  // The document.createElement() function in  is used to create a new HTML element. It allows you to dynamically generate new HTML elements and add them to the page.
   // Add each entry from the history to the history display
   history.forEach(calculation => {
-    // The createElement() method creates an element node.
     const calculationEl = document.createElement('div');
-    calculationEl.textContent = `${calculation.expression} = ${calculation.result}`;
 
-    // The appendChild() function is used to add an element as a child inside a target element (typically an HTML element).
-    // Typically, before using appendChild(), you need to create a new element using createElement(). Then, using appendChild(), the new element is added as a child to the target element.
-    historyEl.appendChild(calculationEl);
-    historyEl.classList.add('inputStyle');
+    // condition
+    if (history.length <= 3) {
+      calculationEl.textContent = `${calculation.expression} = ${calculation.result}`;
+
+      historyEl.appendChild(calculationEl);
+      historyEl.classList.add('inputStyle');
+    } else {
+      historyEl.textContent = 'There is no further history';
+    }
   });
 }
 
